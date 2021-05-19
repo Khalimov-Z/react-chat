fe55c50fc6d6f7e8735f2a05c4811b8b5ebd056b
shishaniyah <arsaevaaisha@gmail>
2021-05-19 16:17:40 +0300

    Вынесла MessageHeader.jsx и MessageButtons.jxs в отдельные компоненты

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import styles from './main.module.css'
-import MessageForm from './MessageForm';
+import SendMessageForm from './SendMessageForm';
 import MessageContainer from './MessageContainer';
+import MessageHeader from './MessageHeader';
 
 function Main () {
   return (
     <div className={styles.main}>
-      <div className={styles['main-contact']}>
-        <div className={styles['search-chat']}>
-          <i className="fas fa-search">{''}</i>
-        </div>
-        <div className={styles['contact-title']}>
-          Кудузов Ахмад
-        </div>
-        <div className={styles['chat-settings']}>
-          <i className="fas fa-cog">{''}</i>
-        </div>
-      </div>
+      <MessageHeader />
       <MessageContainer />
-      <MessageForm />
+      <SendMessageForm />
     </div>
   );
 }
