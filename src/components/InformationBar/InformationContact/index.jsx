556c3a043479422020f6ec2d14dd21910866fe1a
Khalimov-Z <djordlo077@gmail.com>
2021-05-19 20:37:15 +0300

    Разбил InformationBar на компоненты

---
+++ src/components/InformationBar/InformationContact/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './information-contact.module.css';
+
+function InformationContact (props) {
+  return (
+    <div className={styles["information-contact"]}>
+      <div className={styles.avatar}> k</div>
+      <div className={styles.username}> ewrwer</div>
+      <div className={styles.nickname}> @erwrwe</div>
+      <div className={styles.icons}>
+        <span className={styles.icon}><i className="fas fa-phone-alt" /></span>
+        <span className={styles.icon}><i className="fas fa-video"/></span>
+        <span className={styles.icon}><i className="fas fa-envelope"/></span>
+      </div>
+    </div>
+  );
+}
+
+export default InformationContact;
