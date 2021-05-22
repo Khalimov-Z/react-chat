c801feae8f9669a4bcce0b0db6fd785c79a7eaf1
shishaniyah <arsaevaaisha@gmail>
2021-05-22 22:03:38 +0300

    Вывела имена контактов, статус онлайн, буквы на аватарах, работа с редьюсером contacts.js

--- src/components/Sidebar/Contacts.jsx
+++ src/components/Sidebar/Contacts.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './sidebar.module.css';
 import Contact from './Contact';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadContacts } from '../../redux/ducks/contacts';
 
 function Contacts() {
+  const contacts = useSelector((state) => state.contacts.contacts);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadContacts())
+  }, [dispatch]);
+
   return (
     <div className={styles.scroll}>
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
+      {contacts.map(contact => {
+        return <Contact contact={contact} key={contact.id} />
+      })}
     </div>
   );
 }
--- src/redux/ducks/contacts.js
+++ src/redux/ducks/contacts.js
@@ -1,12 +1,45 @@
-const initialState = {};
+const initialState = {
+  contacts: [],
+  lastMessage: {
+    content: '',
+  },
+  loading: false,
+};
 
 export default function contacts(state = initialState, action) {
   switch (action.type) {
+    case CONTACTS_LOAD_START:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case CONTACTS_LOAD_SUCCESS:
+      return {
+        ...state,
+        contacts: action.payload,
+        loading: false,
+      };
+
     default:
       return state;
   }
 }
 
-// тут экшн креэйторы
+const CONTACTS_LOAD_START = 'contacts/load/start';
+const CONTACTS_LOAD_SUCCESS = 'contacts/load/success';
 
-// тут санки
+export const loadContacts = () => {
+  return (dispatch) => {
+    dispatch({ type: CONTACTS_LOAD_START });
+
+    fetch('https://api.intocode.ru:8001/api/contacts')
+      .then((response) => response.json())
+      .then((json) => {
+        dispatch({
+          type: CONTACTS_LOAD_SUCCESS,
+          payload: json,
+        });
+      });
+  };
+};
