5ae96fee4c57d60e9277baa60f25b73ea89d2160
Kasumov-Z <Lord.kasumov@mail.ru>
2021-05-29 16:59:31 +0300

    Код ревью.

--- src/components/Sidebar/Contacts/index.jsx
+++ src/components/Sidebar/Contacts/index.jsx
@@ -6,33 +6,22 @@ import styles from './contacts.module.css';
 function Contacts() {
   const filter = useSelector((state) => state.contacts.filter);
 
-  const contacts = useSelector((state) => state.contacts.contacts.filter(
-    contact => contact.fullname.toLowerCase().indexOf(filter.toLowerCase()) !== -1
-  ));
+  const contacts = useSelector((state) =>
+    state.contacts.contacts.filter(
+      (contact) =>
+        contact.fullname.toLowerCase().indexOf(filter.toLowerCase()) !== -1,
+    ),
+  );
 
-  const loading = useSelector(state => state.contacts.loading)
+  const loading = useSelector((state) => state.contacts.loading);
 
   if (loading) {
-    return (
-      <div>
-        <Contact />
-        <Contact />
-        <Contact />
-        <Contact />
-        <Contact />
-        <Contact />
-        <Contact />
-        <Contact />
-        <Contact />
-      </div>
-    )
+    return <Contact />;
   } else {
     return (
       <div className={styles.scroll}>
-        {contacts.map(contact => {
-          return(
-            <Contact contact={contact} key={contact._id} />
-          )
+        {contacts.map((contact) => {
+          return <Contact contact={contact} key={contact._id} />;
         })}
       </div>
     );
